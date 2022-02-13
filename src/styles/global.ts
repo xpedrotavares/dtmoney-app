9f987d94f82d2d2e87103738654807420f015d2d
xpedrot <xpedrotavares@gmail.com>
2022-02-13 13:50:43 -0300

    Set google fonts link to index.html head and set the font styles for body and bold weight elements in global.ts

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -12,8 +12,6 @@ export const GlobalStyle = createGlobalStyle`
 
 		--background: #f0f2f5;
 		--shape: #FFFFFF;
-		
-
 	}
 
 	* {
@@ -24,18 +22,26 @@ export const GlobalStyle = createGlobalStyle`
 	
 	html {
 		@media (max-width: 1080px) {
-			font-size: 93.75%; // 15px
+			font-size: 93.75%;
 		}
 
 		@media (max-width: 720px) {
-			font-size: 87.5%; // 14px
+			font-size: 87.5%;
 		}
 	}
 
-	// rem = 1rem = font-size da pagina
 	body {
 		background: var(--background);
-		-webkit-font-smoothing: antialiased; // fonts mais nitidas
+		-webkit-font-smoothing: antialiased;
+	}
+
+	body, input, textarea, button {
+		font-family: 'Poppins', sans-serif;
+		font-weight: 400;
+	}
+
+	h1, h2, h3, h4, h5, h6, strong {
+		font-weight: 600;
 	}
 
 	button {
