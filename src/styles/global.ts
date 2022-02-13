bebe2542cf1a31ea44f9a0f216ed5d9efd504557
xpedrot <xpedrotavares@gmail.com>
2022-02-13 13:32:47 -0300

    Add color scheme to global.ts styles

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,6 +1,20 @@
 import { createGlobalStyle } from 'styled-components'
 
 export const GlobalStyle = createGlobalStyle`
+	:root {
+		--red: #E52E4D;
+		--blue: #5429CC;
+
+		--blue-light: #6933FF;
+
+		--text-title: #363F5F;
+		--text-body: #969CB3;
+
+		--background: #f0f2f5;
+		--shape: #FFFFFF;
+		
+
+	}
 
 	* {
 		margin: 0;
